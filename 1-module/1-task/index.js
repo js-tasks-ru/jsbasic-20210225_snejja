41f72912c23fdc5762900cbc70e24f4dbe50e88f
Snejja <may06@mail.ru>
2021-03-02 20:41:46 +0300

    Added Homework 1-module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
-function factorial(n) {
-  // ваш код...
+function factorial(n) {  
+  let result = 1;
+
+  if (n === 0 || n === 1)
+  {
+    return result;
+  }
+  else {
+    for (let i = n; i >= 1; i--) {
+      result *= i;      
+    }
+    return result;
+  }
 }
