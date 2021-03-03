e61ddba76c613194531300f2a86582609ee6cf0c
Snejja <may06@mail.ru>
2021-03-03 12:39:55 +0300

    Changed 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,14 +1,12 @@
 function factorial(n) {  
   let result = 1;
 
-  if (n === 0 || n === 1)
-  {
+  if (n === 0 || n === 1) {
     return result;
   }
-  else {
-    for (let i = n; i >= 1; i--) {
-      result *= i;      
-    }
-    return result;
+  
+  for (let i = n; i >= 1; i--) {
+    result *= i;      
   }
+  return result;
 }
