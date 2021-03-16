fbd7b3e11e8c0b27eac600eb441dd1d36aa662a0
Snejja <may06@mail.ru>
2021-03-16 20:38:02 +0300

    Added Homework 4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,18 @@
 function highlight(table) {
-  // ваш код...
+  let dataPersons = table.querySelector('tbody');
+
+  for (let i = 0; i < dataPersons.rows.length; i++) {
+    const cellAge = dataPersons.rows[i].cells[1];
+    const cellGender = cellAge.nextElementSibling;
+    const cellStatus = cellGender.nextElementSibling;    
+
+    if (cellStatus.hasAttribute('data-available')) {        
+      dataPersons.rows[i].classList.add(cellStatus.dataset.available === 'true' ? 'available' : 'unavailable');
+    } else {
+      dataPersons.rows[i].setAttribute("hidden", "true");
+    }
+
+    dataPersons.rows[i].classList.add(cellGender.innerHTML === "m" ? "male" : "female");
+    dataPersons.rows[i].style = Number(cellAge.innerHTML) < 18 ? "text-decoration: line-through" : "";
+  } 
 }
